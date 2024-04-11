a3313579fe8b350677f7509db115e44a93977916
Nate Hall <nhall1@dm9dwk7762.lan>
2024-04-11 13:13:48 -0400

    fix silly error

--- src/main/java/org/example/springdatasandbox/Controller.java
+++ src/main/java/org/example/springdatasandbox/Controller.java
@@ -1,9 +1,11 @@
 package org.example.springdatasandbox;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/test")
+@RestController
+@RequestMapping("/test")
 public class Controller {
     private final MyEntityRepo repository;
 
